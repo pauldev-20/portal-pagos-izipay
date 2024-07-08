1fd1a5337133a0a757714cfa37b3b7059dcf190f
Paul Mallqui Rivera <mallquiriverap@gmail.com>
2024-07-08 02:41:07 +0000

    fix: it fixed file docker-compose.yml

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -1,7 +1,7 @@
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Municipalidad Distrital de Santa María</title>
+<title>Portal de Pagos Izipay</title>
 <link rel="icon" href="{{ asset('images/logo_1.png') }}" type="image/x-icon" />
 
 <link rel="stylesheet" href="{{ asset('js/bootstrap.min.js') }}">
